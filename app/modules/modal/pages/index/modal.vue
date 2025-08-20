03abe5cf0881ac539a3d18ee57a77a7b0b0e0f2e
Szymon Klimek <klimek.s.katowice@gmail.com>
2025-08-20 23:49:31 +0200

    handle close modal

--- app/modules/modal/pages/index/modal.vue
+++ app/modules/modal/pages/index/modal.vue
@@ -1,15 +1,22 @@
 <template>
-    <v-dialog v-model="showModal" width="400">
+    <v-dialog :model-value="true" @update:model-value="onDialogUpdate" width="400">
         <v-card>
             <v-card-title>Modal</v-card-title>
             <v-card-text>Modal content</v-card-text>
             <v-card-actions>
-                <v-btn @click="showModal = false">Close</v-btn>
+                <v-btn @click="closeModal">Close</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 <script setup lang="ts">
-import { ref } from '#imports'
+import { ref,useRouter  } from '#imports'
 const showModal = ref(true)
+const router = useRouter()
+function closeModal() {
+  router.back() // or use router.replace({ path: '/page' }) if you prefer
+}
+function onDialogUpdate(val: boolean) {
+  if (!val) closeModal()
+}
 </script>
